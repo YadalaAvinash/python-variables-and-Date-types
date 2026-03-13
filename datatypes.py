8b0146eb00b10af6628a7e56ba177b16f82744d0
YadalaAvinash <avinashyadala1981@gmail.com>
2026-03-13 21:44:19 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,53 @@
+# data types
+'''
+1.Integer
+2.float
+3.complex
+4.Boolean
+5.List
+6.Tuple
+7.strings
+8.sets
+9.Dictnaroys
+'''
+
+a=23 # int 
+print(a)
+print(type(a))
+print(" == "*20)
+
+b=43.6 # float
+print(b)
+print(type(b))
+print(" == "*20)
+
+c=3+4j # complex
+print(c)
+print(type(c))
+print(" == "*20)
+
+d=True   # boolaen
+print(d)
+print(type(d))
+print(" == "*20)
+
+li=[10,"Avinash",23.4,True,] # list
+print(li)
+print(type(li))
+print(" == "*20)
+
+tle=(80,"python",56.8,True) # tuple
+print(tle)
+print(type(tle))
+print(" == "*20)
+
+dict={"a":"Avinash",2:"Python",} # dictnarorys
+print(dict)
+print(type(dict))
+print(" == "*20)
+
+sets={10,20,40,60,70} # sets
+print(sets)
+print(type(sets))
+
+
---
+++ variables.py
@@ -0,0 +1,23 @@
+# Today topic variables and datatypes
+'''
+roles for variables
+1.variables cannot start with numbers. etc 2apple,
+2.variables cannot cannot any special character  examples : @apple,appple# etc
+3.variable cerate time dont give space
+'''
+# variables 
+a=10 
+print(a) # output is : 10   .variable is cantiner storing the value in the merory, variable use for storing a value
+
+
+
+a=10,20,30
+print(a) # packed
+
+a,b,c=10,20,30
+print(a) # 10
+print(b) # 20
+print(c) #30
+
+a,b,c=10
+print(c) # shows errrror 
